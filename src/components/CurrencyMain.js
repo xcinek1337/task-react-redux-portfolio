f2e782664836032a7bb932e406cdd08f6f5957a3
Marcin <xcinek1337@wp.pl>
2023-11-17 08:28:15 +0100

    https acces restricted

--- src/components/CurrencyMain.js
+++ src/components/CurrencyMain.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+const API_KEY = '2665e52a5c8d9599141c1e1d812b7492';
+
 const CurrencyApp = () => {
 	const fetchApi = () => {
-		fetch('http://api.exchangeratesapi.io/v1/latest?access_key=2665e52a5c8d9599141c1e1d812b7492')
+		fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=${API_KEY}`)
+		// fetch(`https://api.exchangeratesapi.io/v1/latest?access_key=${API_KEY}`)
 			.then(resp => resp.json())
 			.then(data => console.log(data));
 	};
