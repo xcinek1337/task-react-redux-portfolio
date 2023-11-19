70807523c094a731e2a023f237b1d63abfb532c6
Marcin <xcinek1337@wp.pl>
2023-11-19 22:29:48 +0100

    question for mentor in code

--- src/components/FormSelect.js
+++ src/components/FormSelect.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setSelectedCurrencyCodeAction } from './actions/currency';
 
 const FormSelect = () => {
+	//w selekcie uzywam distpatcham a w inputach przesylam do glownego formularza i tam operuje na danych, czy to blad, i powinienem zrobic tak samo z selektem? tzn wyciagnac onChange props i odczytywac selekt value w CurrenyFormPanel? *****************
 	const dispatch = useDispatch();
 	const [selectValue, setSelectValue] = useState('');
 	const currencyCodes = useSelector(state => state.currencyCodes);
