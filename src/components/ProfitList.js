a8648549b6b151d1f6b00a13ea8482569357839c
Marcin <xcinek1337@wp.pl>
2023-11-20 15:52:52 +0100

    init logic for dynamic render table

--- src/components/ProfitList.js
+++ src/components/ProfitList.js
@@ -1,36 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import '../style/profitList.scss';
 
 const ProfitList = () => {
-  return (
-    <div className="profit__wrapper">
-      <table className="profit__table table">
-        <thead>
-          <tr>
-            <th>Currency:</th>
-            <th>Purchase Date:</th>
-            <th>Amount:</th>
-            <th>Exchange Rate on Purchase Day:</th>
-            <th>Exchange Rate Today:</th>
-            <th>Current Investment Value:</th>
-            <th>Profit/Loss:</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>test</td>
-            <td>test</td>
-            <td>test</td>
-            <td>test</td>
-            <td>test</td>
-            <td>test</td>
-            <td>test</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+	const { selectedCode, purchaseDate, amount, oldPrice, todaysPrice, isSubmitValid } = useSelector((state) => state);
+	
+
+	return (
+		<div className='profit__wrapper'>
+			<table className='profit__table table'>
+				<thead>
+					<tr>
+						<th>Currency:</th>
+						<th>Purchase Date:</th>
+						<th>Amount:</th>
+						<th>Exchange Rate on Purchase Day:</th>
+						<th>Exchange Rate Today:</th>
+						<th>Current Investment Value:</th>
+						<th>Profit/Loss:</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>test</td>
+						<td>test</td>
+						<td>test</td>
+						<td>test</td>
+						<td>test</td>
+						<td>test</td>
+						<td>test</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default ProfitList;
